c4ab81b63416dc3b0fbe2df2bcde8b861f6c3473
Javier-Martin-Marquez <javier.martin2305@gmail.com>
2026-01-21 13:41:02 +0100

    Cambios Front y Back
    Hemos puesto el back en una carpeta y el front en otra, además de crear las carpetas y archivos del front

---
+++ front/src/components/ActionBox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ActionBox() {
+  return (
+    <div>ActionBox</div>
+  )
+}
+
+export default ActionBox
---
+++ front/src/pages/SaveSerch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SaveSerch() {
+  return (
+    <div>SaveSerch</div>
+  )
+}
+
+export default SaveSerch
